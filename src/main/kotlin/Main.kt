40650f0a72d63ee98991aa2b398d94c2247a7294
Martin Dreher <luma.inc@gmail.com>
2018-03-22 13:11:05 +0100

    progress

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,13 @@
+
 import com.github.kittinunf.fuel.core.FuelManager
 import com.github.kittinunf.fuel.httpGet
-import com.google.common.reflect.TypeToken
 import com.google.gson.FieldNamingPolicy
 import com.google.gson.GsonBuilder
-import com.linkedin.paldb.api.PalDB
 import de.merc.pokekotlin.model.Generation
 import de.merc.pokekotlin.model.NamedApiResourceList
 import de.merc.pokekotlin.model.PokemonSpecies
-import org.mapdb.DBMaker
-import org.mapdb.Serializer
-import org.xerial.snappy.SnappyInputStream
-import org.xerial.snappy.SnappyOutputStream
 import java.io.File
-import java.sql.DriverManager
-import java.util.*
 import java.util.concurrent.TimeUnit
-import java.util.logging.Level
-import java.util.logging.Logger
-import java.util.zip.GZIPInputStream
-import java.util.zip.GZIPOutputStream
 import kotlin.system.measureNanoTime
 
 
@@ -42,335 +31,102 @@ fun main(args: Array<String>) {
 
 
     logTiming("main") {
-        NetworkCache("cache.db").use { cache ->
-            val species = cache.loadAll<PokemonSpecies>("https://pokeapi.co/api/v2/pokemon-species/")
-            species.groupingBy { it.generation.url }.eachCount().entries.sortedBy { it.key }.forEach { println("${it.key} => ${it.value}") }
+        val cache = NetworkCache(File("cache.txt"))
+        val species = cache.loadAll<PokemonSpecies>("https://pokeapi.co/api/v2/pokemon-species/").values
 
-            println("-------------------")
+        // compareWithGenerations(cache, species)
+        // printGen5(species)
 
-            val generations = cache.loadAll<Generation>("https://pokeapi.co/api/v2/generation/")
-            generations.forEach {
-                println("https://pokeapi.co/api/v2/generation/${it.id}/ => ${it.pokemonSpecies.size}")
+        val byName = species.asSequence().map { it.getName().toLowerCase() to it }.toMap()
+
+        val teams = mutableListOf<List<PokemonSpecies>>()
+        ClassLoader.getSystemResourceAsStream("doubles.txt")
+                .bufferedReader()
+                .forEachLine { line ->
+            val pokemonNames = line.split("\\W+".toRegex()).mapNotNull { byName[it.toLowerCase()] }
+            if (pokemonNames.size % 4 != 0) {
+                println("warning: uneven number of pokemon ${pokemonNames.size}, skipping this line:\n> $line")
+            } else {
+                for (i in 0 until pokemonNames.size step 4) {
+                    teams.add(pokemonNames.subList(i, i + 4))
+                }
+            }
+        }
+
+
+        println()
+        println("team stats")
+        for ((team, count) in teams
+                .groupingBy { it }
+                .eachCount()
+                .entries
+                .sortedByDescending { e -> e.value }) {
+            println("${team.map { it.getName() }}: $count")
+        }
+        println()
+
+        println("---------------")
+        val generations = cache.loadAll<Generation>("https://pokeapi.co/api/v2/generation/")
+        for (team in teams) {
+            val gens = team.map { generations[it.generation.url]!!.id}
+            val maxGen = gens.max()!!
+            if (maxGen <= 5) {
+                println("${team.map { it.getName() }}: ${gens.min()} - $maxGen")
             }
         }
     }
 }
+
+private fun compareWithGenerations(cache: NetworkCache, species: List<PokemonSpecies>) {
+    species.groupingBy { it.generation.url }
+            .eachCount()
+            .entries
+            .sortedBy { it.key }
+            .forEach { println("${it.key} => ${it.value}") }
+
+    println("-------------------")
+
+    val generations = cache.loadAll<Generation>("https://pokeapi.co/api/v2/generation/")
+    for ((url, generation) in generations.entries) {
+        println("$url => ${generation.pokemonSpecies.size}")
+    }
+}
+
+private fun printGen5(species: List<PokemonSpecies>) {
+    for (s in species) {
+        if (s.generation.url.endsWith("5/")) {
+            println("%4d %-20s %s".format(s.id,
+                    s.getName(),
+                    s.getName("de")))
+        }
+    }
+}
+
+fun PokemonSpecies.getName(languageName: String = "en") = names.first { it.language.name == languageName }.name
 
 inline fun logTiming(label: String, block: () -> Unit) {
     val elapsed = measureNanoTime(block)
     println("> $label in ${"%dms".format(TimeUnit.NANOSECONDS.toMillis(elapsed))} ")
 }
 
-object TestProperties {
-    @JvmStatic
-    fun main(args: Array<String>) {
-        val data = readOldDb("cache.db")
 
-        val propFile = File("cache.properties")
-        if (!propFile.exists()) {
-            logTiming("writing properties") {
-                with(Properties()) {
-                    putAll(data)
-                    store(propFile.bufferedWriter(), "")
-                }
-            }
-        }
-        var other = emptyMap<String, String>()
-        logTiming("reading properties") {
-            with(Properties()) {
-                load(propFile.bufferedReader())
-                other = toMap() as Map<String, String>
-            }
-        }
+class NetworkCache(val file: File) {
+    val cache = mutableMapOf<String, String>()
 
-        println("${data.size} => ${other.size} : ${data == other}")
-    }
-}
-
-
-object TestPlain {
-    @JvmStatic
-    fun main(args: Array<String>) {
-        val data = readOldDb("cache.db")
-
-        val propFile = File("cache.txt")
-        if (!propFile.exists()) {
-            logTiming("writing txt") {
-                propFile.bufferedWriter().use { wr ->
-                    data.forEach { key, value ->
-                        wr.append(key).append(0.toChar()).append(value).append('\n')
-                    }
-                }
-            }
-        }
-        val other = mutableMapOf<String, String>()
-        logTiming("reading txt") {
-            propFile.useLines {
-                it.forEach {
-                    val (key, value) = it.split(0.toChar(), limit = 2)
-                    other[key] = value
-                }
-            }
-        }
-
-        println("${data.size} => ${other.size} : ${data == other}")
-        if (data != other) {
-            println(other)
-            propFile.delete()
+    init {
+        file.forEachLine { line ->
+            val (key, value) = line.split('\u0000', limit = 2)
+            cache[key] = value
         }
     }
-}
 
-
-object TestPlainGzip {
-    @JvmStatic
-    fun main(args: Array<String>) {
-        val data = readOldDb("cache.db")
-
-        val propFile = File("cache.txt.gz")
-        if (!propFile.exists()) {
-            logTiming("writing txt.gz") {
-                GZIPOutputStream(propFile.outputStream()).bufferedWriter().use { wr ->
-                    data.forEach { key, value ->
-                        wr.append(key).append(0.toChar()).append(value).append('\n')
-                    }
-                }
-            }
-        }
-        val other = mutableMapOf<String, String>()
-        logTiming("reading txt.gz") {
-            GZIPInputStream(propFile.inputStream()).bufferedReader().useLines {
-                it.forEach {
-                    val (key, value) = it.split(0.toChar(), limit = 2)
-                    other[key] = value
-                }
-            }
-        }
-
-        println("${data.size} => ${other.size} : ${data == other}")
-        if (data != other) {
-            println(other)
-            propFile.delete()
-        }
-    }
-}
-
-
-object TestPalDB {
-    @JvmStatic
-    fun main(args: Array<String>) {
-        val data = readOldDb("cache.db")
-        val palFile = File("cache-compressed.paldb")
-        Logger.getLogger("com.linkedin.paldb").level = Level.OFF
-        val conf = PalDB.newConfiguration().set("compression.enabled", "true")
-        if (!palFile.exists()) {
-            logTiming("writing paldb") {
-                with(PalDB.createWriter(palFile, conf)) {
-                    data.forEach { key, value -> put(key, value) }
-                    close()
-                }
-            }
-        }
-        var other = mutableMapOf<String, String>()
-        logTiming("reading paldb") {
-            with(PalDB.createReader(palFile, conf)) {
-                iterable<String, String>().forEach { e -> other[e.key] = e.value }
-                close()
-            }
-        }
-
-        println("${data.size} => ${other.size} : ${data == other}")
-        if (data != other) {
-            println(other)
-            palFile.delete()
-        }
-    }
-}
-
-
-object TestSqlite {
-    @JvmStatic
-    fun main(args: Array<String>) {
-        val data = readOldDb("cache.db")
-        val sqliteFile = File("cache-sqlite.db")
-        if (!sqliteFile.exists()) {
-            logTiming("writing sqlite") {
-                DriverManager.getConnection("jdbc:sqlite:$sqliteFile")?.use { con ->
-                    con.createStatement().use { stmt ->
-                        stmt.executeUpdate("create table cache (key text not null primary key, value text not null)")
-                    }
-                    con.prepareStatement("insert into cache values(?, ?)").use { stmt ->
-                        data.forEach { key, value ->
-                            stmt.setString(1, key)
-                            stmt.setString(2, value)
-                            stmt.executeUpdate()
-                        }
-                    }
-                }
-            }
-        }
-        var other = mutableMapOf<String, String>()
-        logTiming("reading sqlite") {
-            DriverManager.getConnection("jdbc:sqlite:$sqliteFile")?.use { con ->
-                con.createStatement().use { stmt ->
-                    stmt.executeQuery("select key, value from cache").use { rs ->
-                        while (rs.next()) {
-                            other[rs.getString(1)] = rs.getString(2)
-                        }
-                    }
-                }
-            }
-        }
-
-        println("${data.size} => ${other.size} : ${data == other}")
-        if (data != other) {
-            println(other)
-            sqliteFile.delete()
-        }
-    }
-}
-
-
-object TestGson {
-    @JvmStatic
-    fun main(args: Array<String>) {
-        val data = readOldDb("cache.db")
-        val gsonFile = File("cache.json")
-        if (!gsonFile.exists()) {
-            logTiming("writing gson") {
-                val gson = GsonBuilder().create()
-                gsonFile.bufferedWriter().use { wr ->
-                    gson.toJson(data, wr)
-                }
-            }
-        }
-
-        var other = emptyMap<String, String>()
-        logTiming("reading gson") {
-            val gson = GsonBuilder().create()
-
-            try {
-                gsonFile.bufferedReader().use { reader ->
-                    other = gson.fromJson(reader, object : TypeToken<HashMap<String, String>>() {}.type)
-                }
-            } catch (e: Exception) {
-                println(e)
-            }
-
-        }
-
-        println("${data.size} => ${other.size} : ${data == other}")
-        if (data != other) {
-            println(other)
-            gsonFile.delete()
-        }
-    }
-}
-
-
-object TestGsonCompressed {
-    @JvmStatic
-    fun main(args: Array<String>) {
-        val data = readOldDb("cache.db")
-        val gsonFile = File("cache.json.gz")
-        if (!gsonFile.exists()) {
-            logTiming("writing gson") {
-                val gson = GsonBuilder().create()
-                GZIPOutputStream(gsonFile.outputStream()).bufferedWriter().use { wr ->
-                    gson.toJson(data, wr)
-                }
-            }
-        }
-
-        var other = emptyMap<String, String>()
-        logTiming("reading gson") {
-            val gson = GsonBuilder().create()
-
-            try {
-                GZIPInputStream(gsonFile.inputStream()).bufferedReader().use { reader ->
-                    other = gson.fromJson(reader, object : TypeToken<HashMap<String, String>>() {}.type)
-                }
-            } catch (e: Exception) {
-                println(e)
-            }
-
-        }
-
-        println("${data.size} => ${other.size} : ${data == other}")
-        if (data != other) {
-            println(other)
-            gsonFile.delete()
-        }
-    }
-}
-
-object TestGsonSnappy {
-    @JvmStatic
-    fun main(args: Array<String>) {
-        val data = readOldDb("cache.db")
-        val gsonFile = File("cache.json.sn")
-        if (!gsonFile.exists()) {
-            logTiming("writing gson") {
-                val gson = GsonBuilder().create()
-                SnappyOutputStream(gsonFile.outputStream()).bufferedWriter().use { wr ->
-                    gson.toJson(data, wr)
-                }
-            }
-        }
-
-        var other = emptyMap<String, String>()
-        logTiming("reading gson") {
-            val gson = GsonBuilder().create()
-
-            try {
-                SnappyInputStream(gsonFile.inputStream()).bufferedReader().use { reader ->
-                    other = gson.fromJson(reader, object : TypeToken<HashMap<String, String>>() {}.type)
-                }
-            } catch (e: Exception) {
-                println(e)
-            }
-
-        }
-
-        println("${data.size} => ${other.size} : ${data == other}")
-        if (data != other) {
-            println(other)
-            gsonFile.delete()
-        }
-    }
-}
-
-fun readOldDb(file: String): Map<String, String> {
-    var map = emptyMap<String, String>()
-    logTiming("reading old db") {
-        DBMaker.fileDB(file)
-                .concurrencyDisable()
-                .fileMmapEnableIfSupported()
-                .make()
-                .use { db ->
-                    map = db.hashMap("cache", Serializer.STRING, Serializer.STRING)
-                            .createOrOpen()
-                            .toMap()
-                }
-    }
-    return map
-}
-
-
-class NetworkCache(file: String) : AutoCloseable {
-    private val db = DBMaker.fileDB(file)
-            .concurrencyDisable()
-            .fileMmapEnableIfSupported()
-            .make()
-
-    val cache = db.hashMap("cache", Serializer.STRING, Serializer.STRING)
-            .createOrOpen()
-
-    val gson = GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create()
+    val gson = GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create()!!
 
     inline fun <reified T> loadJson(url: String): T {
         return gson.fromJson(cache.computeIfAbsent(url) {
-            url.httpGet().responseString().third.get()
+            val json = url.httpGet().responseString().third.get()
+            file.appendText("$url\u0000$json\n")
+            json
         }, T::class.java)
     }
 
@@ -379,19 +135,15 @@ class NetworkCache(file: String) : AutoCloseable {
         return if (list.results.size >= list.count) list else loadJson(url + "?limit=${list.count}")
     }
 
-    inline fun <reified T> loadAll(url: String): List<T> {
+    inline fun <reified T> loadAll(url: String): Map<String, T> {
         val list = loadResourceList(url)
         check(list.count == list.results.size) {
             "count (${list.count}) should be equal to the length of the results array (${list.results.size})"
         }
 
         return list.results.map {
-            loadJson<T>(it.url)
-        }
-    }
-
-    override fun close() {
-        db.close()
+            it.url to loadJson<T>(it.url)
+        }.toMap()
     }
 
 }
