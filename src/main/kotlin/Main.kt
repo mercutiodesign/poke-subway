e1de96d7348a0457518b9b4f91d744a604f81c23
Martin Dreher <luma.inc@gmail.com>
2018-03-28 11:57:48 +0200

    created berry checker

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,31 +3,39 @@ import com.github.kittinunf.fuel.core.FuelManager
 import com.github.kittinunf.fuel.httpGet
 import com.google.gson.FieldNamingPolicy
 import com.google.gson.GsonBuilder
-import de.merc.pokekotlin.model.Generation
-import de.merc.pokekotlin.model.NamedApiResourceList
-import de.merc.pokekotlin.model.PokemonSpecies
+import de.merc.pokekotlin.model.*
+import org.apache.poi.ss.util.CellRangeAddress
+import org.apache.poi.xssf.usermodel.XSSFCellStyle
+import org.apache.poi.xssf.usermodel.XSSFRow
+import org.apache.poi.xssf.usermodel.XSSFWorkbook
+import java.awt.Desktop
 import java.io.File
 import java.net.URLEncoder
 import java.util.concurrent.TimeUnit
 import kotlin.system.measureNanoTime
 
 
-fun main(args: Array<String>) {
-    logTiming("fuel startup") {
-        with(FuelManager.instance) {
-            baseHeaders = mapOf(
-                    "User-Agent" to "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36"
-            )
-            addRequestInterceptor {
-                { request -> println("loading ${request.url}"); it(request) }
-            }
-            addResponseInterceptor {
-                { request, response ->
-                    println("done loading ${request.url}, status: ${response.statusCode}, length: ${response.contentLength}")
-                    it(request, response)
-                }
+fun setupFuel() {
+    with(FuelManager.instance) {
+        baseHeaders = mapOf(
+                "User-Agent" to "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36"
+        )
+        addRequestInterceptor {
+            { request -> println("loading ${request.url}"); it(request) }
+        }
+        addResponseInterceptor {
+            { request, response ->
+                println("done loading ${request.url}, status: ${response.statusCode}, length: ${response.contentLength}")
+                it(request, response)
             }
         }
+    }
+}
+
+
+fun main(args: Array<String>) {
+    logTiming("fuel startup") {
+        setupFuel()
     }
 
 
@@ -88,7 +96,7 @@ fun main(args: Array<String>) {
                 .forEach { (species, count) ->
                     println("%-15s | %-70s | %-15s | %2d".format(
                             species.getName("de"),
-                            "https://bulbapedia.bulbagarden.net/wiki/${URLEncoder.encode(species.getName(), "UTF-8")}#Game_locations",
+                            "https://bulbapedia.bulbagarden.net/wiki/${species.getName().urlEncode()}#Game_locations",
                             species.generation.name,
                             count))
                 }
@@ -123,10 +131,99 @@ private fun printGen5(species: List<PokemonSpecies>) {
 }
 
 fun PokemonSpecies.getName(languageName: String = "en") = names.first { it.language.name == languageName }.name
+fun Item.getName(languageName: String = "en") = names.first { it.language.name == languageName }.name
 
 inline fun logTiming(label: String, block: () -> Unit) {
     val elapsed = measureNanoTime(block)
     println("> $label in ${"%dms".format(TimeUnit.NANOSECONDS.toMillis(elapsed))} ")
+}
+
+object BerryChecker {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        setupFuel()
+        val cache = NetworkCache(File("cache.txt"))
+        val berries = cache.loadAll<Berry>("https://pokeapi.co/api/v2/berry/")
+        println("found ${berries.size} berries")
+
+        val headings = listOf("url", "name en", "name de", "growth time", "max harvest", "soil dryness", "smoothness")
+        val colOffset = headings.size
+        val flavorNames = berries.values
+                .asSequence()
+                .flatMap { it.flavors.asSequence() }
+                .map { it.flavor.name }
+                .toSortedSet()
+                .mapIndexed { index, name -> name to index + colOffset }
+                .toMap()
+
+        println(flavorNames)
+
+        val output = File("berries.xlsx")
+
+        XSSFWorkbook().use { wb ->
+            val sheet = wb.createSheet("data")
+
+            val bold = wb.createCellStyle()
+            bold.setFont(wb.createFont().also { it.bold = true })
+
+            var rowNum = 0
+
+            val header = sheet.createRow(rowNum++)
+            headings.forEachIndexed { col, value ->
+                header.createCell(col, value, bold)
+            }
+            flavorNames.forEach { name, col ->
+                header.createCell(col, name, bold)
+            }
+
+            berries.forEach { url, berry ->
+                val row = sheet.createRow(rowNum++)
+                row.createCellLink(0, url, url.substringAfter("/v2"))
+
+                val item = cache.loadJson<Item>(berry.item.url)
+                val name = item.getName()
+                row.createCellLink(1,"https://bulbapedia.bulbagarden.net/wiki/" + name.urlEncode(), name)
+                row.createCell(2, item.getName("de"))
+                row.createCellInt(3, berry.growthTime)
+                row.createCellInt(4, berry.maxHarvest)
+                row.createCellInt(5, berry.soilDryness)
+                row.createCellInt(6, berry.smoothness)
+
+                berry.flavors.forEach {
+                    row.createCellInt(flavorNames[it.flavor.name]!!, it.potency)
+                }
+            }
+
+            sheet.setAutoFilter(CellRangeAddress(0, rowNum - 1, 0, colOffset + flavorNames.size - 1))
+            headings.indices.asSequence().drop(2).forEach(sheet::autoSizeColumn)
+            sheet.setColumnWidth(0, sheet.getColumnWidth(2))
+            sheet.setColumnWidth(1, sheet.getColumnWidth(2))
+            output.outputStream().use(wb::write)
+        }
+
+        Desktop.getDesktop().open(output)
+    }
+}
+
+// url encoding for actual urls not forms
+fun String.urlEncode() = URLEncoder.encode(this, "UTF-8").replace("+", "%20")
+
+fun XSSFRow.createCell(col: Int, value: String, style: XSSFCellStyle? = null) {
+    createCell(col).apply {
+        setCellValue(value)
+        cellStyle = style
+    }
+}
+
+fun XSSFRow.createCellLink(col: Int, url: String, value: String) {
+    createCell(col).apply {
+        setCellValue(value)
+        cellFormula = "HYPERLINK(\"$url\", \"$value\")"
+    }
+}
+
+fun XSSFRow.createCellInt(col: Int, value: Int) {
+    createCell(col).setCellValue(value.toDouble())
 }
 
 
@@ -161,9 +258,9 @@ class NetworkCache(val file: File) {
             "count (${list.count}) should be equal to the length of the results array (${list.results.size})"
         }
 
-        return list.results.map {
+        return list.results.associate {
             it.url to loadJson<T>(it.url)
-        }.toMap()
+        }
     }
 
 }
